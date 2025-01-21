3f21be04fc6b4de3bfad4042745f72b96d2d8c1d
Channa Ly <channa.info@gmail.com>
2025-01-21 20:34:07 +0700

    Close #28 run test sequentially

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,15 +1,15 @@
 // Load environment variables
 import './src/libs/env';
-import sequelize from './src/config/database';
+import dbClient from './src/config/database';
 
 // Ensure the database is synced before all tests
 beforeAll(async () => {
-  await sequelize.sync({ force: true });
+  await dbClient.sync({ force: true });
 });
 
 // Clean up the database after each test
 afterEach(async () => {
-  const models = Object.values(sequelize.models);
+  const models = Object.values(dbClient.models);
   for (const model of models) {
     try {
       await model.destroy({ where: {}, truncate: true, cascade: true });
@@ -23,5 +23,5 @@ afterEach(async () => {
 
 // Close the database connection after all tests
 afterAll(async () => {
-  await sequelize.close();
+  await dbClient.close();
 });
